0aaa5ffe14e48ea9ca6fb0ef2a87b36256f5dbba
windowsrefund <windowsrefund@gmail.com>
2011-08-30 16:32:05 -0400

    syntax

--- manifests/group.pp
+++ manifests/group.pp
@@ -1,7 +1,7 @@
 # This definition creates a group entry via the sudoers type. 
 # We use this in order to pull in the module's init.pp
 #
-define sudoers::group(ensure = 'present', $nopasswd = false, $commands) {
+define sudoers::group($ensure = 'present', $nopasswd = false, $commands) {
 
   include sudoers
   include sudoers::params
--- manifests/user.pp
+++ manifests/user.pp
@@ -1,7 +1,7 @@
 # This definition creates a user entry via the sudoers type. 
 # We use this in order to pull in the module's init.pp
 #
-define sudoers::user(ensure = 'present', $nopasswd = false, $commands) {
+define sudoers::user($ensure = 'present', $nopasswd = false, $commands) {
 
   include sudoers
   include sudoers::params
