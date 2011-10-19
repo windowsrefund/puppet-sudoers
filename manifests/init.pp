12baa0cfc97cd69b8709a74037b8c5444acbd120
Mike Crute <mcrute@gmail.com>
2011-10-18 23:11:17 -0400

    pacakges is not required to use Package resource

--- manifests/init.pp
+++ manifests/init.pp
@@ -3,8 +3,7 @@ class sudoers {
   include sudoers::params
   include sudoers::os
 
-  # Virtual package resources are defined in a seperate 'packages' module
-  include packages
   realize(Package[$sudoers::params::packages])
+  realize Package[$sudoers::params::packages]
 
 }
