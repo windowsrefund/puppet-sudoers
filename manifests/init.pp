2333c79a82385d6d560ea6136b6149093dee9278
Mike Crute <mcrute@gmail.com>
2011-10-24 21:42:45 -0400

    Ensure sudo package is present

--- manifests/init.pp
+++ manifests/init.pp
@@ -3,7 +3,9 @@ class sudoers {
   include sudoers::params
   include sudoers::os
 
-  package { $sudoers::params::packages: }
+  package { $sudoers::params::packages:
+	ensure => present,
+  }
   realize Package[$sudoers::params::packages]
 
 }
